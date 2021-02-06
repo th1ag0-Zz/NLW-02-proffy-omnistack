74b845e445e08dbfb08a21aca65a369d53f4158a
Thiago <thiagoF89.tf@gmail.com>
2021-02-06 11:53:25 -0300

    add code base

---
+++ server/src/controllers/ConnectionsController.ts
@@ -0,0 +1,19 @@
+import { Request, response, Response } from 'express'
+import db from "../database/connection";
+
+export default class ConnectionsController {
+  async index (req: Request, res: Response) {
+    const [totalConnections] = await db('connections').count('* as total')
+
+    const { total } = totalConnections
+    return res.json({ total })
+  }
+
+  async create (req: Request, res: Response) {
+    const { user_id } = req.body
+
+    await db('connections').insert({ user_id })
+  
+    return res.status(201).send()
+  }
+}
---
+++ server/src/database/connection.ts
@@ -0,0 +1,14 @@
+import knex from "knex";
+import path from "path";
+
+
+
+const db = knex({
+  client: "sqlite",
+  connection: {
+    filename: path.resolve(__dirname, "database.sqlite"),
+  },
+  useNullAsDefault: true,
+});
+
+export default db;
